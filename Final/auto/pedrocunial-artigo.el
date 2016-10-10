1359cc024197a11421586ec1437b227dc9e3ea9a
Pedro Cunial <pedrocunial@gmail.com>
2016-10-10 14:29:30 -0300

    Quick commit - Mon Oct 10 14:29:30 BRT 2016

---
+++ Final/auto/pedrocunial-artigo.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "pedrocunial-artigo"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("IEEEtran" "conference")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "brazilian") ("inputenc" "utf8") ("fontenc" "T1")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "IEEEtran"
+    "IEEEtran10"
+    "babel"
+    "inputenc"
+    "fontenc")
+   (LaTeX-add-bibitems
+    "fromarpanet"
+    "internethistory"
+    "framingyears"
+    "twobit"))
+ :latex)
+
