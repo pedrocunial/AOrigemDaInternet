3cc1b836aa3fa265681ad26a55f78a2d73068808
Pedro Cunial <pedrocunial@gmail.com>
2016-11-08 21:44:33 -0200

    done

--- Final/auto/pedrocunial-artigo.el
+++ Final/auto/pedrocunial-artigo.el
@@ -4,7 +4,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("IEEEtran" "conference")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("babel" "brazilian") ("inputenc" "utf8") ("fontenc" "T1")))
+                     '(("babel" "brazilian") ("inputenc" "utf8") ("fontenc" "T1") ("graphicx" "pdftex" "dvips")))
    (TeX-run-style-hooks
     "latex2e"
     "IEEEtran"
@@ -12,13 +12,24 @@
     "babel"
     "inputenc"
     "fontenc"
-    "dirtytalk")
+    "ifpdf"
+    "graphicx")
+   (LaTeX-add-labels
+    "smtp_fig")
    (LaTeX-add-bibitems
     "fromarpanet"
     "internethistory"
     "framingyears"
     "twobit"
     "rfc100"
-    "nsfnet"))
+    "nsfnet"
+    "rfc801"
+    "rfc788"
+    "rfc561"
+    "rfc821"
+    "email"
+    "www"
+    "yahoo"
+    "wired"))
  :latex)
 
